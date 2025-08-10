94391cbc36d91029c29cb1e2b3d253f55063c35f
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-08-11 03:04:03 +0600

    [route]: added login related routes;

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import {
 import { ConfigProvider } from 'antd';
 import enUS from 'antd/locale/en_US';
 
+import { AuthProvider } from './context/AuthContext';
+import { LazyLogin } from './component/auth/LazyLogin';
+import { LazySignup } from './component/auth/LazySignup';
 import { CustomLayout } from './layout/CustomLayout';
 import { LazyLanding } from './component/landing/LazyLanding';
 import { LazyPage2 } from './component/LazyPage2';
@@ -15,19 +18,23 @@ import { LazyPage3 } from './component/LazyPage3';
 
 const App = () => (
   <ConfigProvider locale={enUS}>
-    <BrowserRouter basename="/react-pipeline">
-      <CustomLayout>
-        <Routes>
-          <Route path="/landing" element={<LazyLanding />} />
-          <Route path="/page2" element={<LazyPage2 />} />
-          <Route path="/page3" element={<LazyPage3 />} />
-          <Route
-            path="*"
-            element={<Navigate to="/landing" replace />}
-          />
-        </Routes>
-      </CustomLayout>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter basename="/react-pipeline">
+        <CustomLayout>
+          <Routes>
+            <Route path="/landing" element={<LazyLanding />} />
+            <Route path="/login" element={<LazyLogin />} />
+            <Route path="/signup" element={<LazySignup />} />
+            <Route path="/page2" element={<LazyPage2 />} />
+            <Route path="/page3" element={<LazyPage3 />} />
+            <Route
+              path="*"
+              element={<Navigate to="/landing" replace />}
+            />
+          </Routes>
+        </CustomLayout>
+      </BrowserRouter>
+    </AuthProvider>
   </ConfigProvider>
 );
 
