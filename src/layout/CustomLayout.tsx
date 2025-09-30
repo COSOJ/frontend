fbcfdb370842b83b01debedc9c464687ce8d46e9
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-09-30 22:41:26 +0600

    [route]: integrated submission to menu;

--- src/layout/CustomLayout.tsx
+++ src/layout/CustomLayout.tsx
@@ -15,6 +15,10 @@ const defaultMenus: MenuDataItem[] = [
     name: 'Problems',
   },
   {
+    path: '/submissions',
+    name: 'Submissions',
+  },
+  {
     path: '/',
     name: 'Pages',
     children: [
