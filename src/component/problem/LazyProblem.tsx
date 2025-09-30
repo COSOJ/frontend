789cbe102bf550b6618344519d9f7d9d9aa2542a
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-09-30 20:49:46 +0600

    [fix]: removed props for problem in suspense;;

--- src/component/problem/LazyProblem.tsx
+++ src/component/problem/LazyProblem.tsx
@@ -4,12 +4,7 @@ const Problem = lazy(() => import('./Problem'));
 
 const LazyProblem = () => (
     <Suspense fallback={<div>Loading...</div>}>
-        <Problem
-            problemDescription='This is a sample problem description for the Problem component.'
-            tags={['Arrays', 'Data Structures']}
-            title='2D Array - DS'
-
-        />
+        <Problem />
     </Suspense>
 );
 
