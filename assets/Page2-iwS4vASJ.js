d3e8534250e00bf98127bccad32ad6b0c39eb959
maifeeulasad <maifeeulasad@users.noreply.github.com>
2026-02-03 16:18:43 +0000

    deploy: f9e3e32ade83f9a613d689fc906ef70ca8cb44a4

--- assets/Page2-iwS4vASJ.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-0GV9pv74.js";import{e as s}from"./index-BGU-i8DA.js";const n=()=>{const[t]=s.usePersistentState("global/counter",0);return e.jsxs("div",{children:["Page 2",e.jsxs("div",{style:{border:"1px solid red",padding:"10px"},children:["global/counter state: ",t]})]})};export{n as default};
